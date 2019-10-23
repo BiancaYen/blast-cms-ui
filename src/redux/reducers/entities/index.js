dfbba305b290725363bfdaf65ad5004a05be3ce6
BiancaN <BiancaN@BiancaNicholson-KJDigital.local>
2019-10-23 10:13:07 +0200

    Remove entities edit reducer

--- src/redux/reducers/entities/index.js
+++ src/redux/reducers/entities/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 
 import create from './createReducer';
-import edit from './editReducer';
 import index from './indexReducer';
 
 const entities = combineReducers({
     create,
-    edit,
     index
 });
 
